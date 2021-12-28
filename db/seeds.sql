234b9f0cd4794f750ce6a067d77d6a869f6ba2f4
Sihayah <sihaya_a@hotmail.com>
2021-12-28 04:00:01 -0500

    test seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO departments (name)
+VALUES ('design'), ('development'), ('management');
+
+INSERT INTO roles (job_title, salary)
+VALUES ('UI/UX designer', 75000), ('Graphic Designer', 72000), ('VS Designer', 80000), ('Front-end Developer', 70000), ('Back-end developer', 80000), ('Full stack developer', 90000), ('Office manager', 65000), ('Department head', 90000);
+
+INSERT INTO employees (first_name, last_name)
+VALUES ('Millie', 'Moss'), ('Xena', 'Patel'), ('Yoshi', 'Ito'),('Yuki', 'Nakamura'), ('Bernice', 'Olivier'), ('Margarita', 'Gonzalez'), ('Issa', 'Ahmed'), ('Fatima', 'Baquri'), ('Ali', 'Arain'), ('Prisha', 'Das'), ('Guillermo', 'Rodriguez'), ('Sofi', 'West'),('Ha-yoon', 'Park'), ('Do-Yun', 'Kim');
+
+SELECT*FROM departments;
+SELECT* FROM roles;
+SELECT* FROM employess;
