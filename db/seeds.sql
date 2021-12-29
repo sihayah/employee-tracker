ca48a5ceca9f4ce7744e02bf740255dcf82e1cde
Sihayah <sihaya_a@hotmail.com>
2021-12-29 12:58:23 -0500

    select statements created

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO departments (name)
+INSERT INTO departments (dept_name)
 VALUES ('design'), ('development'), ('management');
 
 INSERT INTO roles (job_title, salary, department_id)
@@ -6,11 +6,3 @@ VALUES ('UI/UX designer', 75000, 1), ('Graphic Designer', 72000, 1), ('Front-end
 
 INSERT INTO employees (first_name, last_name, department_id, role_id)
 VALUES ('Millie', 'Moss', 1, 1), ('Xena', 'Patel', 1, 1), ('Yoshi', 'Ito', 1, 2),('Yuki', 'Nakamura', 1, 2), ('Bernice', 'Olivier', 2, 3), ('Margarita', 'Gonzalez', 2, 3), ('Issa', 'Ahmed', 2, 4), ('Fatima', 'Baquri', 2, 4), ('Ali', 'Arain', 2, 4), ('Prisha', 'Das', 2, 4), ('Guillermo', 'Rodriguez', 3, 5), ('Sofi', 'West', 3, 6),('Ha-yoon', 'Park', 3, 6), ('Do-Yun', 'Kim', 3, 6);
-
-SELECT *FROM departments;
-SELECT * FROM roles;
-SELECT employees.*, roles.job_title
-                AS title
-                FROM employees
-                LEFT JOIN roles
-                ON employees.role_id = roles.id;
---
+++ db/selections.sql
@@ -0,0 +1,11 @@
+SELECT roles.id, job_title, salary, dept_name
+FROM roles
+    JOIN departments
+    ON roles.department_id = departments.id;
+
+SELECT employees.id, last_name, first_name, job_title, dept_name
+FROM employees
+    JOIN roles
+    ON employees.role_id = roles.id
+    JOIN departments
+    ON roles.department_id = departments.id;
